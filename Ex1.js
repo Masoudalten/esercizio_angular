8e7459295f8b99ed6ff1ca6cde755cbcee7df596
PROD\mbahmanyar <masoud.bahmanyar@alten.it>
2023-12-05 10:27:28 +0100

    fetch optimization

--- Ex1.js
+++ Ex1.js
@@ -31,22 +31,42 @@ function textColor(){
 
 
 
-fetch('https://jsonplaceholder.typicode.com/comments/14')
-    .then(response => response.json())
-    .then(function(comments){
-        let postid = comments.postId;
-        fetch(`https://jsonplaceholder.typicode.com/posts/${postid}`)
-            .then(response => response.json())
-            .then(function(posts){
-            let userid = posts.userId
-            fetch(`https://jsonplaceholder.typicode.com/users/${userid}`)
-                .then(response => response.json())
-                .then(function(users){
-                console.log(users.name)
-            }) 
-        })  
-    })
+// fetch('https://jsonplaceholder.typicode.com/comments/14')
+//     .then(response => response.json())
+//     .then(function(comments){
+//         let postid = comments.postId;
+//         fetch(`https://jsonplaceholder.typicode.com/posts/${postid}`)
+//             .then(response => response.json())
+//             .then(function(posts){
+//             let userid = posts.userId
+//             fetch(`https://jsonplaceholder.typicode.com/users/${userid}`)
+//                 .then(response => response.json())
+//                 .then(function(users){
+//                 console.log(users.name)
+//             }) 
+//         })  
+//     })
 
+
+async function getUserInfo() {
+    try {
+        const commentsResponse = await fetch('https://jsonplaceholder.typicode.com/comments/14');
+        const comments = await commentsResponse.json();
+
+        const postsResponse = await fetch(`https://jsonplaceholder.typicode.com/posts/${comments.postId}`);
+        const posts = await postsResponse.json();
+
+        const usersResponse = await fetch(`https://jsonplaceholder.typicode.com/users/${posts.userId}`);
+        const users = await usersResponse.json();
+
+        console.log(users.name);
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
+
+getUserInfo();
+    
 
 
 fetch('http://jsonplaceholder.typicode.com/comments/14').then(function(response) {
