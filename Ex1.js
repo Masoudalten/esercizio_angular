f5afb9e3246642a0320da2e7fd7e54930fcee4a8
PROD\mbahmanyar <masoud.bahmanyar@alten.it>
2023-12-06 11:47:09 +0100

    callback hell fixed

--- Ex1.js
+++ Ex1.js
@@ -1,14 +1,9 @@
 document.querySelector('h1').style.color = "red";
 
 //EX_2
-// document.getElementById('change_color').addEventListener('click', function(){
-//     document.querySelector('h1').style.color = "blue";
-// });
-
-// while (true) {
-//     console.log("hello")
-// }
-
+document.getElementById('change_color').addEventListener('click', function(){
+    document.querySelector('h1').style.color = "blue";
+});
 
 //EX_3
 
@@ -18,7 +13,7 @@ let timeOut = setInterval(textColor, 2000);
 function textColor(){
     document.querySelector('h1').style.color = isTextColor1 ? "red" : "green";
     isTextColor1 = !isTextColor1;
-    // console.log("color changed");
+    //console.log("color changed");
     
 }
 
@@ -31,48 +26,47 @@ function textColor(){
 
 
 
-// fetch('https://jsonplaceholder.typicode.com/comments/14')
-//     .then(response => response.json())
-//     .then(function(comments){
-//         let postid = comments.postId;
-//         fetch(`https://jsonplaceholder.typicode.com/posts/${postid}`)
-//             .then(response => response.json())
-//             .then(function(posts){
-//             let userid = posts.userId
-//             fetch(`https://jsonplaceholder.typicode.com/users/${userid}`)
-//                 .then(response => response.json())
-//                 .then(function(users){
-//                 console.log(users.name)
-//             }) 
-//         })  
-//     })
-
-// fetch('http://jsonplaceholder.typicode.com/comments/14').then(function(response) {
-//   response.json().then(function(comments){
-//       let postid = comments.postId;
-//   });
-  
-// }).catch(err => console.error(err));
-
-
-async function getUserInfo() {
-    try {
-        const commentsResponse = await fetch('https://jsonplaceholder.typicode.com/comments/14');
-        const comments = await commentsResponse.json();
-
-        const postsResponse = await fetch(`https://jsonplaceholder.typicode.com/posts/${comments.postId}`);
-        const posts = await postsResponse.json();
-
-        const usersResponse = await fetch(`https://jsonplaceholder.typicode.com/users/${posts.userId}`);
-        const users = await usersResponse.json();
-
-        console.log(users.name);
-    } catch (error) {
+fetch('https://jsonplaceholder.typicode.com/comments/14')
+    .then(response => response.json())
+    .then(function(comments){
+        let postid = comments.postId;
+        return fetch(`https://jsonplaceholder.typicode.com/posts/${postid}`)
+    })
+    .then(response => response.json())
+    .then(function(posts){
+        let userid = posts.userId
+        return fetch(`https://jsonplaceholder.typicode.com/users/${userid}`) 
+    }) 
+    .then(response => response.json())
+    .then(function(users){
+        console.log(users.name)
+    })
+    .catch(error => {
         console.error('Error:', error);
-    }
-}
+    });
+            
+    
 
-getUserInfo();
+
+
+// async function getUserInfo() {
+//     try {
+//         const commentsResponse = await fetch('https://jsonplaceholder.typicode.com/comments/14');
+//         const comments = await commentsResponse.json();
+
+//         const postsResponse = await fetch(`https://jsonplaceholder.typicode.com/posts/${comments.postId}`);
+//         const posts = await postsResponse.json();
+
+//         const usersResponse = await fetch(`https://jsonplaceholder.typicode.com/users/${posts.userId}`);
+//         const users = await usersResponse.json();
+
+//         console.log(users.name);
+//     } catch (error) {
+//         console.error('Error:', error);
+//     }
+// }
+
+// getUserInfo();
     
 
 
